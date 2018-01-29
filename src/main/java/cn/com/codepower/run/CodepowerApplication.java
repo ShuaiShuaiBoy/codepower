9c3477a14b036e7b5f657c56c0569fe50a8de895
Shuaishuai <cjs19960625@outlook.com>
2018-01-29 17:19:45 +0800

    初始框架

---
+++ src/main/java/cn/com/codepower/run/CodepowerApplication.java
@@ -0,0 +1,12 @@
+package cn.com.codepower.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodepowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodepowerApplication.class, args);
+	}
+}
